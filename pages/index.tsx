8b11a5c52ce8c4c9efae48c958bbb54f7f4e2426
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-19 20:20:49 +0100

    init: next project with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SmartChat</title>
+        <meta name="description" content="Chat with open AI" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
