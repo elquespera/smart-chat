a89a397d3796fb894314c3509c7fc8a1bb270608
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-25 15:05:18 +0100

    fix: sign in and up urls'

--- components/SignInOrUp.tsx
+++ components/SignInOrUp.tsx
@@ -1,18 +1,18 @@
 import { SignIn, SignUp } from "@clerk/nextjs";
 import { useRouter } from "next/router";
-import Header from "./Header";
 
-const singUpUrl = "/sign-up";
+const signInUrl = "/sign-in";
+const signUpUrl = "/sign-up";
 
 export default function SignInOrUp() {
   const { pathname } = useRouter();
 
   return (
     <div className="h-[100dvh] flex items-center justify-center">
-      {pathname === singUpUrl ? (
-        <SignUp signInUrl="/" />
+      {pathname === signUpUrl ? (
+        <SignUp signInUrl={signInUrl} />
       ) : (
-        <SignIn signUpUrl={singUpUrl} />
+        <SignIn signUpUrl={signUpUrl} />
       )}
     </div>
   );
