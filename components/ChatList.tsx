6cbdb334a581af020472e3a790f13fabc7e8a501
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-27 15:01:22 +0200

    feat: chat list styles

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,6 +1,7 @@
 import { Chat } from "@prisma/client";
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import IconButton from "./IconButton";
 
 interface ChatListProps {
@@ -10,6 +11,8 @@ interface ChatListProps {
 }
 
 export default function ChatList({ chats, open, onChatDelete }: ChatListProps) {
+  const { asPath } = useRouter();
+
   const handleChatDelete = (id: string) => {
     if (onChatDelete) onChatDelete(id);
   };
@@ -17,23 +20,33 @@ export default function ChatList({ chats, open, onChatDelete }: ChatListProps) {
   return (
     <div
       className={clsx(
-        `absolute top-0 z-10 p-4
+        `absolute top-0 z-10 px-2 py-4
          sm:relative flex-shrink-0
-         w-side-menu h-full
+         w-side-menu h-full shadow-xl sm:shadow-none
          transition-transform sm:transition-none
          bg-background border-r border-divider`,
         !open && "-translate-x-[100%] sm:translate-x-0"
       )}
     >
-      <ul>
+      <ul className="flex flex-col gap-1">
         {chats.map(({ title, id }) => (
-          <li key={id} className="relative flex items-center">
-            <Link href={`/${id}`}>{title}</Link>
-            <IconButton
-              icon="close"
-              className="absolute right-0"
-              onClick={() => handleChatDelete(id)}
-            />
+          <li key={id}>
+            <Link
+              href={`/${id}`}
+              className={clsx(
+                "relative overflow-hidden isolate rounded-md flex p-2 w-full bg-background hover:text-contrast hover:bg-primary",
+                asPath === `/${id}` &&
+                  "before:absolute before:inset-0 before:bg-primary before:opacity-10 before:-z-10"
+              )}
+            >
+              <span className="flex-grow">{title}</span>
+              <IconButton
+                icon="close"
+                className="flex-shrink-0"
+                title="Delete chat"
+                onClick={() => handleChatDelete(id)}
+              />
+            </Link>
           </li>
         ))}
       </ul>
