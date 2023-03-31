85fd8b274d1cf35d8196e292859bb7eb67d46be3
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-03-31 12:41:25 +0200

    feat: add icon button hover state

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -8,7 +8,17 @@ interface IconButtonProps extends React.ComponentPropsWithoutRef<"button"> {
 
 export default function IconButton({ icon, ...props }: IconButtonProps) {
   return (
-    <button {...props} className={clsx("disabled:opacity-60", props.className)}>
+    <button
+      {...props}
+      className={clsx(
+        `relative overflow-hidden rounded-full
+        p-2
+        disabled:opacity-60 disabled:hover:before:bg-transparent
+        before:absolute before:inset-0 before:opacity-10
+        hover:before:bg-primary`,
+        props.className
+      )}
+    >
       <Icon type={icon} />
     </button>
   );
